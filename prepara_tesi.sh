222680326f074afc2c4906c6f8ee3ddef540580a
mrizzoli <marco@rizzoli.me.uk>
2015-10-25 12:38:03 +0100

    first

---
+++ prepara_tesi.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e  # Fail on errors
+set -x  # Verbosity all the way
+
+# get token
+## da sistemate usando variabili criptate in .travis.yml
+
+### curl https://bitbucket.org/site/oauth2/access_token -d grant_type=client_credentials -u bSNv6FyLLwsr7jMzC7:pT3TAGUVcBBqMqyhD5BCEcwDTnvmg8kc
+
+## Crea repo da bitbucket
+##mkdir -p markdown
+##git clone https://mrizzoli@bitbucket.org/mrizzoli/tesi.git markdown/
+
+## Setta variabili e percorsi
+
+origine=$PWD
+temporanea=$PWD/"tmp"
+dest=$PWD/"Chapters"
+
+##concatena file md con note, uno per uno
+mkdir -p ${temporanea}
+mkdir -p ${dest}
+
+cd ${origine}/"markdown"
+
+for file in Chapter*
+do
+    echo "copio "$file" in "$temporanea""
+    cat "$file" note.md >> ${temporanea}/$file
+done
+
+##esporta in latex
+
+cd ${temporanea}
+
+for file in *
+do
+    echo "esporto in latex "$file""
+    pandoc --biblatex --chapters -o ${dest}/$file.tex $file
+done
+
+##cambia dir in /tesi/latex
+
+cd ${dest}
+
+for file in *
+do
+    mv $file ${file%%.*}.tex
+done
+
+##xelatex
+
+cd ${origine}
+xelatex template_tesi.tex
+
